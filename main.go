b9d63ce54782a3f365c0d5a99e89aac91ea726e7
cnini <cynthia-zoely-nirina.rabemanantsoa@estiam.com>
2023-11-16 12:28:09 +0100

    Add() method

--- main.go
+++ main.go
@@ -1,5 +1,19 @@
 package main
 
-func main() {
+import "fmt"
 
+func Add(d map[string]string, key string, value string) {
+	d[key] = value
+}
+
+func main() {
+	d := make(map[string]string)
+
+	// Add to map
+	Add(d, "pomme", "apple")   // d["pomme"] = "apple"
+	Add(d, "banane", "banana") // d["banane"] = "banana"
+	Add(d, "voiture", "car")   // d["voiture"] = "car"
+	Add(d, "poire", "pear")    // d["poire"] = "pear"
+
+	fmt.Println("d:", d)
 }
