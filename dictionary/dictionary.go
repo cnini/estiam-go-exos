59c20b8a18c00b92f80f7d8a5bf3cb97b59da5b2
cnini <cynthia-zoely-nirina.rabemanantsoa@estiam.com>
2023-11-16 14:07:57 +0100

    call dictionary package methods

--- dictionary/dictionary.go
+++ dictionary/dictionary.go
@@ -1,1 +1,25 @@
 package dictionary
+
+import "fmt"
+
+func Add(d map[string]string, key string, value string) {
+	d[key] = value
+}
+
+func Get(d map[string]string, key string) (string, error) {
+	if value, exists := d[key]; exists {
+		return value, nil
+	} else {
+		return "", fmt.Errorf("Value for %s key does not exist in dictionary", key)
+	}
+}
+
+func Remove(d map[string]string, key string) (string, error) {
+	_, exists := Get(d, key)
+	if exists != nil {
+		return "", exists
+	} else {
+		delete(d, key)
+		return key + " key deleted", nil
+	}
+}
--- main.go
+++ main.go
@@ -1,40 +1,21 @@
 package main
 
-import "fmt"
-
-func Add(d map[string]string, key string, value string) {
-	d[key] = value
-}
-
-func Get(d map[string]string, key string) (string, error) {
-	if value, exists := d[key]; exists {
-		return value, nil
-	} else {
-		return "", fmt.Errorf("Value for %s key does not exist in dictionary", key)
-	}
-}
-
-func Remove(d map[string]string, key string) (string, error) {
-	_, exists := Get(d, key)
-	if exists != nil {
-		return "", exists
-	} else {
-		delete(d, key)
-		return key + " key deleted", nil
-	}
-}
+import (
+	"estiam-go-exos/dictionary"
+	"fmt"
+)
 
 func main() {
 	d := make(map[string]string)
 
 	// Add to map
-	Add(d, "pomme", "apple")   // d["pomme"] = "apple"
-	Add(d, "banane", "banana") // d["banane"] = "banana"
-	Add(d, "voiture", "car")   // d["voiture"] = "car"
-	Add(d, "poire", "pear")    // d["poire"] = "pear"
+	dictionary.Add(d, "pomme", "apple")   // d["pomme"] = "apple"
+	dictionary.Add(d, "banane", "banana") // d["banane"] = "banana"
+	dictionary.Add(d, "voiture", "car")   // d["voiture"] = "car"
+	dictionary.Add(d, "poire", "pear")    // d["poire"] = "pear"
 
 	// Get value from existing key
-	getBananeVal, getBananeErr := Get(d, "banane")
+	getBananeVal, getBananeErr := dictionary.Get(d, "banane")
 	if getBananeErr != nil {
 		fmt.Println("Get banane (error):", getBananeErr.Error())
 	} else {
@@ -42,7 +23,7 @@ func main() {
 	}
 
 	// Get value from non-existing key
-	getFraiseVal, getFraiseErr := Get(d, "fraise")
+	getFraiseVal, getFraiseErr := dictionary.Get(d, "fraise")
 	if getFraiseErr != nil {
 		fmt.Println("Get fraise (error):", getFraiseErr.Error())
 	} else {
@@ -50,7 +31,7 @@ func main() {
 	}
 
 	// Remove key/value pair from existing key
-	removeVoitureMess, removeVoitureErr := Remove(d, "voiture")
+	removeVoitureMess, removeVoitureErr := dictionary.Remove(d, "voiture")
 	if removeVoitureErr != nil {
 		fmt.Println("Remove voiture (error):", removeVoitureErr.Error())
 	} else {
@@ -58,7 +39,7 @@ func main() {
 	}
 
 	// Remove key/value pair from non-existing key
-	removeFraiseMess, removeFraiseErr := Remove(d, "fraise")
+	removeFraiseMess, removeFraiseErr := dictionary.Remove(d, "fraise")
 	if removeFraiseErr != nil {
 		fmt.Println("Remove fraise (error):", removeFraiseErr.Error())
 	} else {
