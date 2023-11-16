39924377c3771d1ef4ea1bf82574732e91b45464
cnini <cynthia-zoely-nirina.rabemanantsoa@estiam.com>
2023-11-16 12:50:19 +0100

    Get() method with existing key and non-existing key

--- main.go
+++ main.go
@@ -6,6 +6,14 @@ func Add(d map[string]string, key string, value string) {
 	d[key] = value
 }
 
+func Get(d map[string]string, key string) (string, error) {
+	if value, exists := d[key]; exists {
+		return value, nil
+	} else {
+		return "", fmt.Errorf("Value for %s key does not exist in dictionary", key)
+	}
+}
+
 func main() {
 	d := make(map[string]string)
 
@@ -15,5 +23,19 @@ func main() {
 	Add(d, "voiture", "car")   // d["voiture"] = "car"
 	Add(d, "poire", "pear")    // d["poire"] = "pear"
 
-	fmt.Println("d:", d)
+	// Get value from existing key
+	bVal, bErr := Get(d, "banane")
+	if bErr != nil {
+		fmt.Println("banane (error):", bErr.Error())
+	} else {
+		fmt.Println("banane:", bVal)
+	}
+
+	// Get value from non-existing key
+	fVal, fErr := Get(d, "fraise")
+	if fErr != nil {
+		fmt.Println("fraise (error):", fErr.Error())
+	} else {
+		fmt.Println("fraise:", fVal)
+	}
 }
